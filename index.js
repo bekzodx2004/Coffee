b416b73cf51b0daa446af3d4db7ab4fb37836a25
bekzod <bekzodxojaniyazov2004@gmail.com>
2022-09-09 11:26:01 +0500

    JS da yangilanish

--- index.js
+++ index.js
@@ -1,63 +1,67 @@
-yopiq = true;
+yopiq1 = true;
+yopiq2 = true;
+yopiq3 = true;
+yopiq4 = true;
+yopiq5 = true;
 body = document.querySelectorAll(".dropdown__body");
 aylan = document.querySelectorAll("#aylan");
 
 function ochil1() {
-  if (yopiq) {
+  if (yopiq1) {
     body[0].classList.add("ochiq");
     aylan[0].classList.add("aylan");
-    yopiq = false;
+    yopiq1 = false;
   } else {
     body[0].classList.remove("ochiq");
     aylan[0].classList.remove("aylan");
-    yopiq = true;
+    yopiq1 = true;
   }
 }
 
 function ochil2() {
-  if (yopiq) {
+  if (yopiq2) {
     body[1].classList.add("ochiq");
     aylan[1].classList.add("aylan");
-    yopiq = false;
+    yopiq2 = false;
   } else {
     body[1].classList.remove("ochiq");
     aylan[1].classList.remove("aylan");
-    yopiq = true;
+    yopiq2 = true;
   }
 }
 
 function ochil3() {
-  if (yopiq) {
+  if (yopiq3) {
     body[2].classList.add("ochiq");
     aylan[2].classList.add("aylan");
-    yopiq = false;
+    yopiq3 = false;
   } else {
     body[2].classList.remove("ochiq");
     aylan[2].classList.remove("aylan");
-    yopiq = true;
+    yopiq3 = true;
   }
 }
 
 function ochil4() {
-  if (yopiq) {
+  if (yopiq4) {
     body[3].classList.add("ochiq");
     aylan[3].classList.add("aylan");
-    yopiq = false;
+    yopiq4 = false;
   } else {
     body[3].classList.remove("ochiq");
     aylan[3].classList.remove("aylan");
-    yopiq = true;
+    yopiq4 = true;
   }
 }
 
 function ochil5() {
-  if (yopiq) {
+  if (yopiq5) {
     body[4].classList.add("ochiq");
     aylan[4].classList.add("aylan");
-    yopiq = false;
+    yopiq5 = false;
   } else {
     body[4].classList.remove("ochiq");
     aylan[4].classList.remove("aylan");
-    yopiq = true;
+    yopiq5 = true;
   }
 }
